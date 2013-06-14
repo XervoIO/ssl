6de119f4ef0f9de3609f7020949ea1fb3dec1b2e
Erin Thompson <erin@modulus.io>
2013-06-14 13:31:21 -0400

    added documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+VERIFY-SSL
+==========
+
+Simple ssl certificate verification for node.js
+
+## Docs
+You can check out openssl documentatin at [openSSL] (http://www.openssl.org/docs/apps/openssl.html).
+
+## Simple Usage
+
+var ssl = require('ssl');
+var cert = 'cert.pem';
+
+ssl.verify(cert, function(err, status) {
+  if(err) {
+    console.log(err);
+  }
+  console.log('this certificate is ok');
+});
--- ssl.js
+++ ssl.js
@@ -1,8 +1,23 @@
 var fs = require('fs');
 var exec = require('child_process').exec, child;
 var ssl = {};
+/*********************************************
 
+  Verify SSl Certificates
+  Change string to file
+  convert files from .pem to .der (vise versa)
+  remove passcode on private keys
 
+***********************************************/
+
+/***************************************************************************************
+
+  Changing a .pem to .der
+  @param {string} file name of file to change must include .pem
+  @prarm {string} give a file name to save the contents to must include .der
+  @param {function} callback throw an error if command does not execute properly or null
+
+******************************************************************************************/
 ssl.toDER = function(file, derFileName, callback) {
   exec('openssl x509 -in ' + file + ' -outform der -out ' + derFileName, function(error, stdout) {
     if(error) {
@@ -13,6 +28,15 @@ ssl.toDER = function(file, derFileName, callback) {
   })
 };
 
+/***************************************************************************************
+
+  Changing a .der to .pem
+  @param {string} file name of file to change must include .der
+  @prarm {string} give a file name to save the contents to must include .pem
+  @param {function} callback throw an error if command does not execute properly or null
+
+******************************************************************************************/
+
 ssl.toPEM = function(file, pemFileName, callback) {
   exec('openssl x509 -in ' + file + ' -outform pem -out ' + pemFileName, function(error, stdout) {
     if(error) {
@@ -21,6 +45,17 @@ ssl.toPEM = function(file, pemFileName, callback) {
     callback(null);
   })
 };
+
+/***************************************************************************************
+
+  Simple command to verify a certificate
+  **IMPORTANT NOTE CERTIFICATE MUST BE A .PEM FILE
+  @param {string} file name of certificate must include .pem
+  @param {function} callback throw an error if command does not execute properly or if the
+                    certificate is not verified - if the certificate is verified it will
+                    return a string.
+
+******************************************************************************************/
 
 ssl.verify = function(file, callback) {
   exec('openssl verify ' + file, function(error, stdout, stderr) {
@@ -37,13 +72,31 @@ ssl.verify = function(file, callback) {
       index = line.indexOf(' ');
       var firstWord = line.substring(0, index);
       if(firstWord === 'error') {
-        return callback(line);
+        return callback(line, null);
       }
       index = remaining.indexOf('\n');
     }
-    callback(null);
+    callback(null, "this certificate has been verified");
   })
 };
+
+/****************************************************************************************
+
+  Simple command to remove a passphrase from a key and rewrites back to the original file
+  @param  {string} file name of certificate must include extension
+  @param  {string} pass key password
+  @param  {object} opts optional parameters
+  @config {string} [opts.newKeyName] specify the new files name with extension .pem or
+                                     default to newPrivateKey.pem
+  @config {string} [opts.inform]     specify the original key files extension or
+                                     default to PEM
+  @config {string} [opts.inform]     specify the new key files extension or
+                                     default to PEM
+  @param {function} callback throw an error if command does not execute properly, if there
+                    is an error reading or writing the files
+
+******************************************************************************************/
+
 
 ssl.removePasspharse = function(file, pass, opts, callback) {
   if(typeof opts === 'function') {
@@ -56,11 +109,9 @@ ssl.removePasspharse = function(file, pass, opts, callback) {
   exec('openssl rsa -passin pass:' + pass + ' -inform  ' + opts.informExt + ' -in '+ file + ' -outform '+ opts.outformExt + ' -out ' +  opts.newKeyName
     , function(error, stdout) {
     if(error) {
-      console.log('removePasspharse Error :', error);
       callback(error);
     }
-    console.log('passphrase removed');
-    fs.readFileSync('newPrivateKey.pem', function(err, data) {
+    fs.readFileSync(opts.newKeyName, function(err, data) {
       if(err) {
         return callback(err);
       }
@@ -76,6 +127,22 @@ ssl.removePasspharse = function(file, pass, opts, callback) {
   return callback(null);
 };
 
+/****************************************************************************************
+
+  Simple command to write a string to a file to use the functions above
+  @param  {string} string encrypted certificate or key
+  @param  {object} opts optional parameters
+  @config {string} [opts.folderName] specify the new folder name or
+                                     default to temp
+  @config {string} [opts.name]       specify the files name or
+                                     default to temp
+  @config {string} [opts.inform]     specify the files extension or
+                                     default to .pem
+  @param {function} callback throw an error if making folder or writing a file does or
+                    returns the filename
+
+******************************************************************************************/
+
 
 ssl.toFile = function(string, opts, callback) {
   if(typeof opts === 'function') {
@@ -87,7 +154,7 @@ ssl.toFile = function(string, opts, callback) {
   opts.ext = opts.ext || '.pem';
   fs.mkdirSync('./' + opts.folderName, function(err) {
     if(err) {
-      return console.log(err);
+      return callback(err, null);
     }
   })
     fs.writeFile(opts.folderName + '/' + opts.name + opts.ext, string, function(err) {
